f1dc0171c096aa3e81c1e79a4a768ce79a946638
daon1213 <diaeiyo1213@naver.com>
2022-04-14 09:29:18 +0900

    Article Model commit!

---
+++ app/src/main/java/com/daon/usedtrade_part3_06/home/ArticleModel.kt
@@ -0,0 +1,11 @@
+package com.daon.usedtrade_part3_06.home
+
+data class ArticleModel(
+    val sellerId: String,
+    val title: String,
+    val createAt: Long,
+    val price: String,
+    val imageUrl: String
+) {
+    constructor(): this("", "", 0, "", "")
+}
