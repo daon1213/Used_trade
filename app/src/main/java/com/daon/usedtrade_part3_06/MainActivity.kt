fa5034efa7768947d3849424b7bc4cf57813e0a8
daon1213 <diaeiyo1213@naver.com>
2022-04-14 09:33:21 +0900

    Main Activity commit!

---
+++ app/src/main/java/com/daon/usedtrade_part3_06/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.daon.usedtrade_part3_06
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.daon.usedtrade_part3_06.chatlist.ChatListFragment
+import com.daon.usedtrade_part3_06.home.HomeFragment
+import com.daon.usedtrade_part3_06.mypage.MyPageFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val homeFragment = HomeFragment()
+        val charListFragment = ChatListFragment()
+        val myPageFragment = MyPageFragment()
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+        replaceFragment(homeFragment)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> replaceFragment(homeFragment)
+                R.id.chatList -> replaceFragment(charListFragment)
+                R.id.myPage -> replaceFragment(myPageFragment)
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .apply {
+                replace(R.id.fragmentContainer, fragment)
+                commit()
+            }
+    }
+
+}
